90f54dfc10904ffc581b21b8a0d5e5f598719487
Rick Lambrechts <rick@rl-webdiensten.nl>
2021-06-18 12:18:43 +0200

    Added vehicle type to occasions table.

--- src/HexonExportServiceProvider.php
+++ src/HexonExportServiceProvider.php
@@ -76,6 +76,12 @@ class HexonExportServiceProvider extends ServiceProvider
                 __DIR__.'/../database/migrations/add_customer_number_to_occasions_table.php.stub' => database_path('migrations/'.date('Y_m_d').'_300000_add_customer_number_to_occasions_table.php'),
             ], 'migrations');
         }
+
+        if (! class_exists('AddVehicleTypeToOccasionsTable')) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/add_vehicle_type_to_occasions_table.php.stub' => database_path('migrations/'.date('Y_m_d').'_400000_add_vehicle_type_to_occasions_table.php.php'),
+            ], 'migrations');
+        }
     }
 
     private function registerRoutes(): void
